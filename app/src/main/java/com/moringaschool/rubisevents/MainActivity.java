1c62f1fc4a6bcc6ba0d2829257cf18c4314daf0e
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-29 01:32:39 +0300

    Updated MAIN Activity

--- app/src/main/java/com/moringaschool/rubisevents/MainActivity.java
+++ app/src/main/java/com/moringaschool/rubisevents/MainActivity.java
@@ -2,13 +2,42 @@ package com.moringaschool.rubisevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+    @BindView(R.id.searchEvent) EditText mSearchEvent;
+    @BindView(R.id.searchButton)  Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mSearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==mSearchButton){
+            String searchInput=mSearchButton.getText().toString();
+                Intent intent=new Intent(MainActivity.this, Events.class);
+                intent.putExtra("searchInput",searchInput);
+                startActivity(intent);
+
+
+        }
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
